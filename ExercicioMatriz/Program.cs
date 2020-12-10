18c465990064d78a6b0e7a2644bea1ffe168811f
chrisAndriotti <andriottichris@gmail.com>
2020-12-10 15:09:51 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMatriz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+
+        }
+    }
+}
